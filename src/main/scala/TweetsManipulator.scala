a42c900702190cb2d96768e7b4481a93b9c30395
AmeerQatmosh <ameerqsh@gmail.com>
2022-11-18 16:12:07 +0200

    String-To-Date

--- src/main/scala/TweetsManipulator.scala
+++ src/main/scala/TweetsManipulator.scala
@@ -1,8 +1,21 @@
 package org.json.tweets
 
+import com.mongodb.spark.MongoSpark
 import org.apache.log4j.BasicConfigurator
 import org.apache.log4j.varia.NullAppender
 import org.apache.spark.sql.SparkSession
+import com.mongodb.spark.config.WriteConfig
+import org.apache.spark.sql.functions.{col, to_timestamp, udf}
+import com.mongodb.MongoClient
+import com.mongodb.client.model
+import com.mongodb.spark.MongoSpark
+import org.apache.spark.sql.SparkSession
+
+
+
+import java.text.SimpleDateFormat
+
+
 
 object TweetsManipulator {
   def main(args: Array[String]): Unit = {
@@ -22,15 +35,40 @@ object TweetsManipulator {
     Mongodb Spark library to insert them into mongodb database in
     a collection called 'tweets'
      */
+    val sc = sparkSession.sparkContext
 
-    val dataset = sparkSession.read
-      .json("data/boulderFloodGeolocatedTweets.json")
+    val dataDF = sparkSession
+      .read
+      .format("json")
+      .load("data/boulderFloodGeolocatedTweets.json")
 
-    dataset.write.format("com.mongodb.spark.sql.DefaultSource")
-      .option("database", "Tweets")
-      .option("collection", "tweets")
-      .mode("append")
-      .save()
+    val confWrite = WriteConfig(
+      Map("collection" -> "tweets" , "writeConcern.w" -> "majority"),
+      Some(WriteConfig(sc))
+    )
+
+    /*
+    2.The timestamp associated with each tweet is to be stored as a
+    Date object, where the timestamp field is to be indexed.
+     */
+
+    val StringToDate = udf((created_at: String) => {
+      val simpleDateFormat = new SimpleDateFormat ("E MMM dd HH:mm:ss Z yyyy")
+      val date = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss")
+      val ConvertToDate = date.format((simpleDateFormat.parse(created_at)))
+      ConvertToDate
+    })
+
+    val dateFormat = StringToDate(col("created_at"))
+    var dataset = dataDF.withColumn("created_at", dateFormat)
+    val timestamp = to_timestamp(col("created_at"), "MM-dd-yyyy HH:mm:ss")
+    dataset = dataset.withColumn("created_at", timestamp)
+    MongoSpark.save(dataset, confWrite)
+
+
+
+
+
 
   }
 }
